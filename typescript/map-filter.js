a59d95277672a05f4a601585c8fdf194ccfd1bc8
Mayank4543 <mayankrathore9897@gmail.com>
2023-12-01 18:01:24 +0530

    Enum

--- typescript/map-filter.js
+++ typescript/map-filter.js
@@ -1,17 +1,78 @@
-var number = [1, 2, 3, 4, 5];
-var numberTostring = number.map(function (currelm) { return currelm.toString(); });
-console.log(numberTostring);
-var product = {
+// const number: number[] = [1, 2, 3, 4, 5];
+// const numberTostring = number.map((currelm) => currelm.toString());
+// console.log(numberTostring)
+// // objects
+// // const obj = {
+// //     name: "Mayank",
+// //     age: 20,
+// //     course: "BCA",
+// //     isStudent: true,
+// //     address: {
+// //         city: "Pune",
+// //         country: "INDIA"
+// //     }
+// // }
+// // console.log(obj.address.country)
+// type Product = {
+//     name: string;
+//     price: number,
+//     Quantity: number
+//     address: {
+//         city: string;
+//         country: string;
+//     }
+// }
+// const product: Product = {
+//     name: "Mayank",
+//     price: 2000,
+//     Quantity: 5,
+//     address: {
+//         city: 'Mainpuri',
+//         country: "India"
+//     }
+// }
+// const calculatetotalPrice = (product: Product) => {
+//     return `${product.name} buying laptop total cost is ${product.price * product.Quantity}`
+// }
+// console.log(calculatetotalPrice(product))
+// // aliass&&&&&&&********
+// call signature 
+// type Student = {
+//     name: string;
+//     age: number;
+//     greet: (country: string) => string;
+// }
+// const student1: Student = {
+//     name: "Mayank",
+//     age: 20,
+//     greet: ((country): string => ` Welcome  My name is ${student1.name}, I am ${student1.age} and i am from ${country}`)
+// }
+// const introduct: (student1: Student) => string = (student: Student): string => {
+//     const { name, age } = student;
+//     return ` Welcome  My name is ${name}, I am ${age} and i am from `
+// }
+// console.log(introduct(student1))
+// console.log(student1.greet("India"))
+var Roles;
+(function (Roles) {
+    Roles["user"] = "user";
+    Roles["admin"] = "admin";
+})(Roles || (Roles = {}));
+var User = {
     name: "Mayank",
-    price: 2000,
-    Quantity: 5,
-    address: {
-        city: 'Mainpuri',
-        country: "India"
-    }
+    email: 'mayank@gmail.com',
+    password: '12345',
+    role: Roles.admin
 };
-var calculatetotalPrice = function (product) {
-    return "".concat(product.name, " buying laptop total cost is ").concat(product.price * product.Quantity);
+var User2 = {
+    name: "Mayank",
+    email: 'mayank@gmail.com',
+    password: '12345',
+    role: Roles.user
 };
-console.log(calculatetotalPrice(product));
-// aliass&&&&&&&********
+var isAdmin = function (user) {
+    var name = user.name, email = user.email, password = user.password, role = user.role;
+    return role === 'admin' ? "".concat(email, " is allow to edit the website") : "".concat(email, " is not allow to edit the website");
+};
+console.log(isAdmin(User));
+console.log(isAdmin(User2));
--- typescript/map-filter.ts
+++ typescript/map-filter.ts
@@ -1,41 +1,88 @@
-const number: number[] = [1, 2, 3, 4, 5];
-const numberTostring = number.map((currelm) => currelm.toString());
-console.log(numberTostring)
-// objects
-// const obj = {
-//     name: "Mayank",
-//     age: 20,
-//     course: "BCA",
-//     isStudent: true,
+// const number: number[] = [1, 2, 3, 4, 5];
+// const numberTostring = number.map((currelm) => currelm.toString());
+// console.log(numberTostring)
+// // objects
+// // const obj = {
+// //     name: "Mayank",
+// //     age: 20,
+// //     course: "BCA",
+// //     isStudent: true,
+// //     address: {
+// //         city: "Pune",
+// //         country: "INDIA"
+// //     }
+// // }
+// // console.log(obj.address.country)
+// type Product = {
+//     name: string;
+//     price: number,
+//     Quantity: number
 //     address: {
-//         city: "Pune",
-//         country: "INDIA"
+//         city: string;
+//         country: string;
 //     }
 // }
-// console.log(obj.address.country)
-type Product = {
+// const product: Product = {
+
+
+//     name: "Mayank",
+//     price: 2000,
+//     Quantity: 5,
+//     address: {
+//         city: 'Mainpuri',
+//         country: "India"
+//     }
+// }
+// const calculatetotalPrice = (product: Product) => {
+//     return `${product.name} buying laptop total cost is ${product.price * product.Quantity}`
+
+// }
+// console.log(calculatetotalPrice(product))
+// // aliass&&&&&&&********
+// call signature 
+// type Student = {
+//     name: string;
+//     age: number;
+//     greet: (country: string) => string;
+// }
+// const student1: Student = {
+//     name: "Mayank",
+//     age: 20,
+//     greet: ((country): string => ` Welcome  My name is ${student1.name}, I am ${student1.age} and i am from ${country}`)
+// }
+// const introduct: (student1: Student) => string = (student: Student): string => {
+//     const { name, age } = student;
+//     return ` Welcome  My name is ${name}, I am ${age} and i am from `
+// }
+// console.log(introduct(student1))
+// console.log(student1.greet("India"))
+enum Roles {
+    user = "user",
+    admin = "admin"
+}
+type LoginDetails = {
     name: string;
-    price: number,
-    Quantity: number
-    address: {
-        city: string;
-        country: string;
-    }
+    email: string;
+    password: string;
+    role: Roles;
 }
-const product: Product = {
-
-
+const User: LoginDetails = {
     name: "Mayank",
-    price: 2000,
-    Quantity: 5,
-    address: {
-        city: 'Mainpuri',
-        country: "India"
-    }
-}
-const calculatetotalPrice = (product: Product) => {
-    return `${product.name} buying laptop total cost is ${product.price * product.Quantity}`
+    email: 'mayank@gmail.com',
+    password: '12345',
+    role: Roles.admin
 
 }
-console.log(calculatetotalPrice(product))
-// aliass&&&&&&&********
+const User2: LoginDetails = {
+    name: "Mayank",
+    email: 'mayank@gmail.com',
+    password: '12345',
+    role: Roles.user
+
+}
+const isAdmin: (user: LoginDetails) => string = (user: LoginDetails): string => {
+    const { name, email, password, role } = user
+    return role === 'admin' ? `${email} is allow to edit the website` : `${email} is not allow to edit the website`
+}
+console.log(isAdmin(User))
+console.log(isAdmin(User2))
