e9aa6184a1bb08469e5757a26de77c724bc35574
Mayank4543 <mayankrathore9897@gmail.com>
2023-12-03 19:02:28 +0530

    github apiproject by using typesscript

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,11 @@
-"use strict";
-let options = 'active';
-let ravi = {
-    name: "ravi",
-    age: 19,
-    course: "bca"
-};
-function getData(key) {
-    return ravi[key];
-}
-console.log(getData('name'));
+// "use strict";
+// let options = 'active';
+// let ravi = {
+//     name: "ravi",
+//     age: 19,
+//     course: "bca"
+// };
+// function getData(key) {
+//     return ravi[key];
+// }
+// console.log(getData('name'));
