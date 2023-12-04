946e95d70c2121674891d1711c94e2629c9e88a6
Mayank4543 <mayankrathore9897@gmail.com>
2023-12-04 23:42:42 +0530

    static

---
+++ typescript/dist/static.js
@@ -0,0 +1,13 @@
+"use strict";
+class MathOperations {
+    static Pi = Math.PI;
+    static add(num1, num2) {
+        return num1 + num2;
+    }
+    static sub(num1, num2) {
+        return num1 + num2;
+    }
+}
+console.log(MathOperations.Pi);
+console.log(MathOperations.add(12, 13));
+console.log(MathOperations.sub(13, 12));
---
+++ typescript/src/static.ts
@@ -0,0 +1,13 @@
+class MathOperations {
+    public static Pi: number = Math.PI;
+    public static add(num1: number, num2: number): number {
+        return num1 + num2;
+    }
+
+    public static sub(num1: number, num2: number): number {
+        return num1 + num2;
+    }
+}
+console.log(MathOperations.Pi)
+console.log(MathOperations.add(12, 13))
+console.log(MathOperations.sub(13, 12))
