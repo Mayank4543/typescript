66307b7b1b55ae4b11d2dc02a6e52008429d5740
Mayank4543 <mayankrathore9897@gmail.com>
2023-12-04 22:23:34 +0530

    inheritance

--- typescript/dist/class.js
+++ typescript/dist/class.js
@@ -8,6 +8,21 @@ class Persons {
         this.age = age;
         this.hobbies = hobbies;
     }
+    introduce() {
+        return `Hi  I m ${this.name} i am ${this.age} years old and my hobbies is${this.hobbies}`;
+    }
+}
+class Student extends Persons {
+    grades;
+    constructor(names, age, hobbies, grades) {
+        super(names, age, hobbies);
+        this.grades = grades;
+    }
+    introduce() {
+        return `Hi  I m ${this.name} i am ${this.age} years old and my hobbies is${this.hobbies} MY grade in tenth class is ${this.grades}`;
+    }
 }
 const person1 = new Persons("Mayank", 18, ["singing", "coding"]);
 console.log(person1);
+const student = new Student("Mayank", 18, ["singing", "coding"], 84);
+console.log(student.introduce());
--- typescript/src/class.ts
+++ typescript/src/class.ts
@@ -8,7 +8,22 @@ class Persons {
         this.hobbies = hobbies;
 
     }
+    introduce(): string {
+        return `Hi  I m ${this.name} i am ${this.age} years old and my hobbies is${this.hobbies}`
+    }
 
+}
+class Student extends Persons {
+    grades: number;
+    constructor(names: string, age: number, hobbies: string[], grades: number) {
+        super(names, age, hobbies);
+        this.grades = grades
+    }
+    introduce(): string {
+        return `Hi  I m ${this.name} i am ${this.age} years old and my hobbies is${this.hobbies} MY grade in tenth class is ${this.grades}`
+    }
 }
 const person1: Persons = new Persons("Mayank", 18, ["singing", "coding"]);
 console.log(person1)
+const student: Student = new Student("Mayank", 18, ["singing", "coding"], 84);
+console.log(student.introduce())
